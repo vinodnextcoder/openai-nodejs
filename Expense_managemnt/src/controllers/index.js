66c0e20123909279bc82fbaf8088995c766650c4
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2023-08-02 12:15:59 +0530

    Delete Expense_managemnt directory

--- Expense_managemnt/src/controllers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.authController = require('./auth.controller');
-module.exports.userController = require('./user.controller');
-module.exports.deptController = require('./dept.controller');
-module.exports.expenseController = require('./expense.controller');
-
--- Expense_managemnt/src/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Dept = require('./dept.model');
-module.exports.Expense = require('./expense.model');
